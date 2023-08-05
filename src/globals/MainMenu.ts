77906958a5ca7f560d52428ef7811f305e913761
Ronan <roleary81@gmail.com>
2023-08-05 21:58:07 +0200

    add basic global for nav structure

---
+++ src/globals/MainMenu.ts
@@ -0,0 +1,68 @@
+import { GlobalConfig } from "payload/types";
+
+const menuColumnBlocks = [];
+
+export const MainMenu: GlobalConfig = {
+  slug: 'mainMenu',
+  access: {
+    read: ({ req: { user } }) => true,
+    update: ({ req: { user } }) => true,
+  },
+
+  admin: {
+    group: 'Admin'
+  },
+  fields: [
+    {
+      name: 'items',
+      type: 'array',
+      fields: [
+        {
+          name: 'type',
+          type: 'radio',
+          defaultValue: 'link',
+          admin: {
+            layout: 'horizontal',
+          },
+          options: [
+            {
+              label: 'Link',
+              value: 'link',
+            },
+            {
+              label: 'Sub-menu',
+              value: 'subMenu',
+            },
+          ],
+        },
+        {
+          name: 'label',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'subMenu',
+          label: false,
+          type: 'group',
+          admin: {
+            condition: (_, { type } = {}) => type === 'subMenu',
+          },
+          fields: [
+            {
+              name: 'blocks',
+              label: 'Menu Blocks',
+              labels: {
+                singular: 'Menu Block',
+                plural: 'Menu Blocks',
+              },
+              type: 'blocks',
+              blocks: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export default MainMenu;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,10 +10,12 @@ import { buildConfig } from 'payload/config'
 import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
 import { Posts } from './collections/Posts'
+import { MainMenu } from './globals/MainMenu'
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
   collections: [Pages, Posts, Media],
+  globals: [MainMenu],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
