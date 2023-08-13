f30301b96c72ac51b0711aef2aae5381293c51eb
Ronan <roleary81@gmail.com>
2023-08-13 13:40:25 +0200

    mongoooo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   reactStrictMode: true,
+  distDir: '/build',
   swcMinify: true,
   images: {
     domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL],
