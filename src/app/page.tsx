e9aca2ff3b183e861d642675369504d40f57b2d7
Ronan <roleary81@gmail.com>
2023-08-13 21:39:07 +0200

    fourth go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,25 @@ import { Stuff } from '../components/layout/Stuff'
 import { FadeIn } from '../components/shared/FadeIn'
 
 export default async function Home() {
-  const home: Page = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/pages?where[slug][equals]=home&depth=2`,
-    { next: { revalidate: 10 } },
-  )
-    .then(res => res.json())
-    .then(res => res?.docs?.[0])
+  // const home: Page = await fetch(
+  //   `${process.env.NEXT_PUBLIC_SERVER_URL}/api/pages?where[slug][equals]=home&depth=2`,
+  //   { next: { revalidate: 10 } },
+  // )
+  //   .then(res => res.json())
+  //   .then(res => res?.docs?.[0])
 
-  if (!home) {
-    return notFound()
-  }
+  // if (!home) {
+  //   return notFound()
+  // }
 
-  const { subtitle, layout } = home
+  // const { subtitle, layout } = home
   
   return (  
     <FadeIn>
       <main className={'page'}>
-        <TitleContainer title={'little'} subtitle={subtitle}/>
+        {/* <TitleContainer title={'little'} subtitle={subtitle}/> */}
         {/* <GalleryGrid /> */}
-        <RenderBlocks content={layout} />
+        {/* <RenderBlocks content={layout} /> */}
         <Stuff />
         <Team />
       </main>
