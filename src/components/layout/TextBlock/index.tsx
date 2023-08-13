7d1372f9a271b9f0cc38e03a7549d725ad072b6a
Ronan <roleary81@gmail.com>
2023-08-13 23:52:44 +0200

    dumbass

--- src/components/layout/TextBlock/index.tsx
+++ src/components/layout/TextBlock/index.tsx
@@ -53,7 +53,7 @@ export const TextBlock = ({ content }: ITextBlock) => {
         <section className="relative lg:px-8">
           <div className="mx-auto max-w-prose text-lg">
             <h1 className="font-black tracking-tighter text-3xl md:text-[3.5rem] mb-6 last:mb-0 uppercase">
-              {content.blockName} {content.displayMeta ? content.subtitle : null}
+              {content.header} {content.displayMeta ? content.subtitle : null}
             </h1>
             <div className="header__underline"></div>
             <RichText content={content.content} className={`text-gray-600 mt-4 article_text`} />
