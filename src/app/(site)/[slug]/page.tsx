43110208766e5d432271de757dc6dfff3079873b
Ronan <roleary81@gmail.com>
2023-08-13 22:39:43 +0200

    go again

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -5,23 +5,23 @@ import { RenderBlocks } from '../../../components/layout/RenderBlocks'
 import { FadeIn } from '../../../components/shared/FadeIn'
 
 export default async function Page({ params }) {
-  // const page: Page = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SERVER_URL}/api/pages?where[slug][equals]=${params.slug}&depth=2`,
-  //   { next: { revalidate: 10 } },
-  // )
-  //   .then(res => res.json())
-  //   .then(res => res?.docs?.[0])
+  const page: Page = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/pages?where[slug][equals]=${params.slug}&depth=2`,
+    { next: { revalidate: 10 } },
+  )
+    .then(res => res.json())
+    .then(res => res?.docs?.[0])
   
-  // if (!page) {
-  //   return notFound()
-  // }
+  if (!page) {
+    return notFound()
+  }
   
-  // const { layout } = page
+  const { layout } = page
   
   return (
     <FadeIn>
       <main className={`page`}>
-        {/* <RenderBlocks content={layout} /> */}
+        <RenderBlocks content={layout} />
         <p>Page</p>
       </main>
     </FadeIn>
--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -6,23 +6,23 @@ import { RenderBlocks } from '../../../components/layout/RenderBlocks'
 import { FadeIn } from '../../../components/shared/FadeIn'
 
 export default async function About() {
-  // const about: Page = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SERVER_URL}/api/pages?where[slug][equals]=about&depth=2`,
-  //   { next: { revalidate: 10 } },
-  // )
-  //   .then(res => res.json())
-  //   .then(res => res?.docs?.[0])
+  const about: Page = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/pages?where[slug][equals]=about&depth=2`,
+    { next: { revalidate: 10 } },
+  )
+    .then(res => res.json())
+    .then(res => res?.docs?.[0])
   
-  // if (!about) {
-  //   return notFound()
-  // }
+  if (!about) {
+    return notFound()
+  }
   
-  // const { layout } = about
+  const { layout } = about
   
   return (
     <FadeIn>
       <main className={`about`}>
-        <p>About</p>
+        <RenderBlocks content={layout} />
       </main>
     </FadeIn>
   )
