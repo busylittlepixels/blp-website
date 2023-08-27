067dda21c2238e48bfdfa6876178481dc214082f
Ronan <roleary81@gmail.com>
2023-08-27 19:58:59 +0200

    shadow

--- src/app/(site)/studio/page.tsx
+++ src/app/(site)/studio/page.tsx
@@ -147,7 +147,7 @@ export default async function Page() {
            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
            
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:h-80">
                 <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
@@ -158,7 +158,7 @@ export default async function Page() {
             </a>
       
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:col-span-2 md:h-80">
                 <img src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000" loading="lazy" alt="Photo by Magicle" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
@@ -169,7 +169,7 @@ export default async function Page() {
             </a>
 
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:col-span-2 md:h-80">
                 <img src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=1000" loading="lazy" alt="Photo by Martin Sanchez" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
@@ -180,7 +180,7 @@ export default async function Page() {
             </a>
 
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:h-80">
                 <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
@@ -191,7 +191,7 @@ export default async function Page() {
             </a>
  
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:h-80">
                 <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Minh Pham" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
@@ -202,7 +202,7 @@ export default async function Page() {
             </a>
 
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:h-80">
                 <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
@@ -212,7 +212,7 @@ export default async function Page() {
                 <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Dicpic</span>
             </a>
             <a href="#"
-                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-xl md:h-80">
                 <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                 <div
