9bc661d4f89492ef2fc47d24d674c6b55a9f8a9c
Ronan <roleary81@gmail.com>
2023-09-14 01:32:46 +0200

    fleerp

--- src/components/layout/Overlap/index.tsx
+++ src/components/layout/Overlap/index.tsx
@@ -33,8 +33,7 @@ export const Overlap = () => {
                     <h1 className="toutHeader--overlap uppercase font-bold">We <span style={{ "color": "#000"}}>Heart </span>Code</h1>
                     <div className="text--overlap text-white mt-6 text-lg leading-8">
                         <p>
-                            Our passion for tech, and programming in general, is evident in every line of code we write. We believe that coding is an art—a symphony of logic and creativity that has the power to innovate and inspire. Our team is driven by the excitement of problem-solving, the joy of crafting elegant solutions, the belief that technology can be a force for good, and the
-                            desire to guide business on their path to technical and commercial success.  
+                            Our passion for tech, for programming and for growth is evident in every line of code we write, every roadmap we draft, and every word of advice and guidance we offer. We believe that coding is an art—a symphony of logic and creativity that has the power to innovate and inspire. We see tech as a driver of progress and growth and a global scale, and our team is driven by the excitement of problem-solving, the joy of crafting elegant solutions, the belief that technology can be a force for good.  
                         </p>
                     </div>
                 </div>
