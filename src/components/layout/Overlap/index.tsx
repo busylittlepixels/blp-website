b4f9df3d0dd18ce91f246608eb6c0199794b9f74
Ronan <roleary81@gmail.com>
2023-09-14 02:43:52 +0200

    fix Overlap

--- src/components/layout/Overlap/index.tsx
+++ src/components/layout/Overlap/index.tsx
@@ -33,7 +33,7 @@ export const Overlap = () => {
                     <h1 className="toutHeader--overlap uppercase font-bold">Your <span style={{ "color": "#000"}}>Tech</span>. Our <span style={{ "color": "#000"}}>Passion </span>.</h1>
                     <div className="text--overlap text-white mt-6 text-lg leading-8">
                         <p>
-                            Our passion for tech, for programming and for online business development and growth is evident - in every line of code we write, every roadmap we draft, every team we establish, and every word of guidance we offer. <br /><br />We believe that coding is an art—a symphony of logic and creativity that has the power to innovate,  inspire and prosper. We see tech as a driver of progress and growth on a global scale, and our team is driven by the excitement of problem-solving, the joy of crafting elegant solutions, the belief that technology can be a force for good.  
+                            Our passion for tech, for programming and for online business development and growth is the cornerstone of everything we do. <br /><br />We believe that coding is an art—a symphony of logic and creativity that has the power to innovate,  inspire and prosper. We see tech as a driver of progress and growth on a global scale, and our team is driven by the excitement of problem-solving, the joy of crafting elegant solutions, the belief that technology can be a force for good.  
                         </p>
                     </div>
                 </div>
