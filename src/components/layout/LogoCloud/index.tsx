8582676bef3a800cf27d6ecaa2a8af2d9645bb29
Ronan <roleary81@gmail.com>
2023-09-14 10:21:57 +0200

    update LogoCloud to be on the safe side|

--- src/components/layout/LogoCloud/index.tsx
+++ src/components/layout/LogoCloud/index.tsx
@@ -11,7 +11,7 @@ export const LogoCloud = ({ content }:any) => {
           <div className="mx-auto max-w-2xl lg:max-w-none">
             <h1 className="font-black tracking-tighter text-3xl md:text-[3.5rem] mb-2 md:mb-2 last:mb-0 uppercase text-white pb-8">SOME PLACES WE{`'`}VE <span style={{"color":"red"}}>PIXELLED</span></h1>
             <div className="header__underline"></div>
-            <div className="mx-auto mt-10 grid grid-cols-4 items-center gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-4">
+            <div className="mx-auto mt-10 grid grid-cols-3 items-center gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-3">
               <Image
                 className="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
                 src="/assets/images/tnw.svg"
@@ -27,14 +27,14 @@ export const LogoCloud = ({ content }:any) => {
                 height={48}
               />
               
-              <Image
+              {/* <Image
                 className="col-span-2 h-1/2 object-contain object-center md:object-right lg:col-span-1 md:ml-6"
                 src="/assets/images/picnic.webp"
                 alt="Picnic Technologies"
                 width={158}
                 height={48}
                 
-              /> 
+              />  */}
               <Image
                 className="col-span-2 max-h-12 w-full object-cover object-center lg:col-span-1 md:ml-6"
                 src="/assets/images/unitopsmedia_white.png"
