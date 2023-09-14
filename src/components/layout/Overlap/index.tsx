c0a60be83b8397bfc8fc55d7f9d8fc039bf2e028
Ronan <roleary81@gmail.com>
2023-09-14 02:33:18 +0200

    weirdness with the production end, but also neither here nor there

--- src/components/layout/Overlap/index.tsx
+++ src/components/layout/Overlap/index.tsx
@@ -30,10 +30,10 @@ export const Overlap = () => {
                     className={`toutCopy--overlap w-full md:w-1/2 ml-auto bg-red`}
                     style={{ "transform": `translateY(${dist}%)`}}// @ts-ignore
                     parallax={parallax}>
-                    <h1 className="toutHeader--overlap uppercase font-bold">We <span style={{ "color": "#000"}}>Heart </span>Code</h1>
+                    <h1 className="toutHeader--overlap uppercase font-bold">Your <span style={{ "color": "#000"}}>Tech</span>. Our <span style={{ "color": "#000"}}>Passion </span>.</h1>
                     <div className="text--overlap text-white mt-6 text-lg leading-8">
                         <p>
-                            Our passion for tech, for programming and for growth is evident in every line of code we write, every roadmap we draft, and every word of advice and guidance we offer. We believe that coding is an art—a symphony of logic and creativity that has the power to innovate and inspire. We see tech as a driver of progress and growth and a global scale, and our team is driven by the excitement of problem-solving, the joy of crafting elegant solutions, the belief that technology can be a force for good.  
+                            Our passion for tech, for programming and for online business development and growth is evident - in every line of code we write, every roadmap we draft, every team we establish, and every word of guidance we offer. <br /><br />We believe that coding is an art—a symphony of logic and creativity that has the power to innovate,  inspire and prosper. We see tech as a driver of progress and growth on a global scale, and our team is driven by the excitement of problem-solving, the joy of crafting elegant solutions, the belief that technology can be a force for good.  
                         </p>
                     </div>
                 </div>
--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import nestedDocs from "@payloadcms/plugin-nested-docs";
 
 dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
+  path: path.resolve(__dirname, './../.env'),
 })
 
 import { buildConfig } from 'payload/config'
