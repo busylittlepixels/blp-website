fde2b34f796a329e2956bf4164e42f05c33902bc
Ronan <roleary81@gmail.com>
2023-10-07 18:49:29 +0200

    enough

---
+++ src/app/(site)/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollTriggerSlide } from "../../../components/layout/ScrollTriggerSlide"
+
+
+export default async function Page() {
+
+    return(
+    <>
+        <ScrollTriggerSlide />
+    </>
+    )
+}
