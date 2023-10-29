9045dadabe0edaa3d47d41a55a0ec857d0bf4b89
Ronan <roleary81@gmail.com>
2023-10-29 22:21:35 +0100

    wrap this up here

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Analytics } from '@vercel/analytics/react';
 export const metadata = {
 	metadataBase: new URL('https://busylittlepixels.com'),
 	title: 'Busy Little Pixels | Fractional CTO Services & Web Development',
-	description: 'Fractional CTO experts. Headless web and app development, devops, project management, digital strategy, media and events',
+	description: 'Busy Little Pixels | We are Fractional CTOs, specialising in roadmapping your technical projects, web and custom app development, devops, project management, digital strategy, media and eCommerce.',
 	keywords: 'Fractional CTO, CTO, Tech Lead, Business Lead, Online Strategy, Mobile Apps, NextJS, PayloadCMS, Craft CMS, Svelte',
 	url: "https://busylittlepixels.com", 
 	images: [
