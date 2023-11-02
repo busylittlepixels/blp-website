a5461c873101474e518353691227faaafd2a8d51
Ronan <roleary81@gmail.com>
2023-11-03 00:39:54 +0100

    temp workaround padding reduction

--- src/components/layout/LogoCloud/index.tsx
+++ src/components/layout/LogoCloud/index.tsx
@@ -5,7 +5,7 @@ export const LogoCloud = ({ content }:any) => {
 
 
     return (
-      <div className="bg-black py-24 sm:py-32">
+      <div className="bg-black py-4 sm:py-8">
         {/* <FadeIn>
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:max-w-none">
