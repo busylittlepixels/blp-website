f1ba41d9ff9ba4bb06b4f0e73ce884270bf82f39
Ronan <roleary81@gmail.com>
2023-11-18 22:58:24 +0100

    guess we can try

--- src/hooks/useGSAPAnimation.ts
+++ src/hooks/useGSAPAnimation.ts
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import imagesLoaded from "imagesloaded"; // Ensure you have imagesLoaded
+const imagesLoaded = require("imagesloaded"); // Ensure you have imagesLoaded/
 
 gsap.registerPlugin(ScrollTrigger);
 
