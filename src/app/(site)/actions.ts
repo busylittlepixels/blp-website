6dcc216f5235ffc147743cfeac12eaefa8a237e9
Ronan <roleary81@gmail.com>
2023-11-19 19:09:53 +0100

    update actions with types 222

--- src/app/(site)/actions.ts
+++ src/app/(site)/actions.ts
@@ -4,6 +4,5 @@ import { redirect } from 'next/navigation';
 
 export const createSignup = async (formData: FormData) => {
   console.log(formData);
-  console.log(formData.get('email'));
   redirect(`/why-blp`) // Navigate to new route
 }
