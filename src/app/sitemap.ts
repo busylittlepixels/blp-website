950d43058425fdf4c0d9b42f416717455bb7caa1
Ronan <roleary81@gmail.com>
2023-11-21 12:12:05 +0100

    update robots and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,48 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://busylittlepixels.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://busylittlepixels.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://busylittlepixels.com/services',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://busylittlepixels.com/why-blp',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://busylittlepixels.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+        url: 'https://busylittlepixels.com/contact',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+    },
+    {
+        url: 'https://busylittlepixels.com/studio',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+    },
+  ]
+}
