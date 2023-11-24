d9e9f3a3677fb5de7b4cb5726a0f654466506abb
Ronan <roleary81@gmail.com>
2023-11-24 12:00:11 +0100

    add...ahem...harry image

--- src/components/layout/Team/index.tsx
+++ src/components/layout/Team/index.tsx
@@ -23,7 +23,7 @@ const people = [
 	{
 		name: 'Harry Laird',
 		role: 'Product and Technology Lead',
-		imageUrl: '/assets/images/harry.png',
+		imageUrl: '/assets/images/harrylaird.webp',
 		bio: 'Praesentium iure error aliquam voluptas ut libero. Commodi placeat sit iure nulla officiis. Ut ex sit repellat tempora. Qui est accusamus exercitationem natus ut voluptas. Officiis velit eos ducimus.',
 		twitterUrl: '#',
 		linkedinUrl: '#',
